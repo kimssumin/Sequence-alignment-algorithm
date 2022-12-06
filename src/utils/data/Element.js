c3a9cfcbfe1741df694f6fd6e7b7723f03d5db4d
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-12-07 01:00:20 +0900

    Feat: 선택자 utils App.js 와 연결

---
+++ src/utils/data/Element.js
@@ -0,0 +1,8 @@
+import { $ } from '../Utils.js';
+
+export const $result = $('#result');
+export const $submit = $('#submit');
+export const $dnaButton = $('#dnaButton');
+export const $proteinButton = $('#proteinButton');
+export const $dnaForm = $('#dna');
+export const $proteinForm = $('#protein');
