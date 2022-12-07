26dee588855c2026cf24421e980660642d068dda
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-12-07 15:35:38 +0900

    Fix: matrix, 읽어오는 file 부분 수정

--- src/utils/File.js
+++ src/utils/File.js
@@ -1,60 +1,51 @@
-import fs from 'fs';
-let lines = [];
+import {
+  blosum45,
+  blosum50,
+  blosum62,
+  blosum80,
+  blosum90,
+} from './substitution_matrix/BLOSUMmatrixs.js';
+import { pam250, pam30, pam70 } from './substitution_matrix/PAMmatrixs.js';
+
+const matrixs = [blosum45, blosum50, blosum62, blosum80, blosum90, pam250, pam30, pam70];
 const matrix_name = [
-  'BLOSUM62',
   'BLOSUM45',
   'BLOSUM50',
+  'BLOSUM62',
   'BLOSUM80',
   'BLOSUM90',
+  'PAM250',
   'PAM30',
   'PAM70',
-  'PAM250',
 ];
-for (let i = 0; i < 8; i++) {
-  let file_path = './substitution_matrix/' + matrix_name[i] + '.txt';
-  lines.push(fs.readFileSync(file_path).toString().split('\n'));
-}
-
-//lines.push(fs.readFileSync('../substitution_matrix/BLOSUM62.txt').toString().split("\n"));
-
 export class files {
   constructor() {
-    this.all_matrix = [];
+    this.all_matrix = {};
   }
 
-  //파일 불러오기
-  blosum62() {
-    this.all_matrix['BLOSUM62'] = [];
-    for (let i = 0; i < lines[0].length - 1; i++) {
-      lines[0][i] = lines[0][i].replace(/\s\s/gi, ' ');
-      //console.log(lines[i])
-      let a = lines[0][i].split(' ');
-
-      a.pop();
-      if (i === 0) {
-        a.shift();
-      }
-      this.all_matrix['BLOSUM62'].push(a);
-      console.log(a);
-    }
+  all_files() {
+    matrixs.forEach((matrix, idx) => {
+      this.all_matrix[matrix_name[idx]] = this.getEachMatrix(matrix);
+    });
+    console.log(this.all_matrix);
   }
 
-  all_file_() {
-    for (let c = 0; c < 8; c++) {
-      this.all_matrix[matrix_name[c]] = [];
-      for (let i = 0; i < lines[c].length - 1; i++) {
-        lines[c][i] = lines[c][i].replace(/\s\s/gi, ' ');
-        //console.log(lines[i])
-        let a = lines[c][i].split(' ');
-
-        a.pop();
-        if (i === 0) {
-          a.shift();
-        }
-        this.all_matrix[matrix_name[c]].push(a);
-        //console.log('>>' + matrix_name[c]);
-        //console.log(a);
+  getEachMatrix(matrix) {
+    const all = matrix.split('\n');
+    all.pop();
+    const matrixArr = all.map((line) => {
+      line = line.replace(/\s\s/gi, ' ');
+      const lineArr = line.split(' ');
+      if (lineArr[0] === '') {
+        lineArr.shift();
       }
-    }
+      lineArr.pop();
+      return lineArr;
+    });
+    matrixArr.splice(0, 1);
+    matrixArr.splice(matrixArr.length - 1, 1);
+    return matrixArr;
   }
 }
+// const file2 = new files();
+// file2.all_files();
